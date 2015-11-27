965db382614bb78f471a16d09f11d1735d278890
Brian Vanderbusch <brian@hackerhappyhour.com>
2015-11-27 15:45:18 -0600

    update comand tests

--- test/lib/command.js
+++ test/lib/command.js
@@ -1,9 +1,6 @@
 /*global expect, assert*/
 var Command = require('../../lib/command');
-var assert = require("chai").assert;
-
 var command = new Command();
-
 
 describe('command prototype',function(){
 
@@ -11,5 +8,21 @@ describe('command prototype',function(){
     expect(command.attach).to.be.a('function');
     expect(command.append).to.be.a('function');
     expect(command.appendFromPath).to.be.a('function');
+  });
+
+  it('should have the following properties:', function(){
+    expect(command.commands).to.be.an('array');
+    expect(command.cliOptions).to.be.an('array');
+  });
+
+});
+
+describe('command usage', function(){
+  it('should allow children commands to be added',function(){
+
+  });
+
+  it('should allow grandchildren to be chained to children', function(){
+
   });
 });
