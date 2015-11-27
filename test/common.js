2beaa69d231cb29199d10a7fd1ae6251b4b73737
Brian Vanderbusch <brian@hackerhappyhour.com>
2015-11-27 15:44:32 -0600

    add sinon to deps

--- test/common.js
+++ test/common.js
@@ -1,3 +1,7 @@
 var chai = require("chai");
 global.expect = chai.expect;
 global.assert = chai.assert;
+global.sinon = require("sinon");
+
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
